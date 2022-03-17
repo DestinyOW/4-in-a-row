f4adeb90233f63aa2060aab08fd29d6d7bf59f93
DestinyOW <guyovadya12345@gmail.com>
2022-03-17 02:53:40 +0200

    Co-authored-by: Ran <Rann42@users.noreply.github.com>

--- Client.py
+++ Client.py
@@ -1,18 +1,47 @@
 import socket
-#import pygame
+import pygame
 
-#pygame.init()
+pygame.init()
 
-#SCR_WIDTH = 1000
-#SCR_HEIGHT = 1000
+SCR_WIDTH = 1000
+SCR_HEIGHT = 1000
+BOARD_COLS = 7
+BOARD_ROWS = 6
 
-#screen = pygame.display.set_mode((SCR_WIDTH,SCR_HEIGHT))
-#pygame.display.set_caption("4 In a Row")
-#screen.fill((229,252,194))
-#pygame.display.flip()
+screen = pygame.display.set_mode((SCR_WIDTH,SCR_HEIGHT))
 
+pygame.display.set_caption("4 In a Row")
+screen.fill((229,252,194))
+pygame.display.flip()
+
+def DrawBoard(screen):
+    rect_width = 10
+    canvas_left_offset = 300
+    canvas_right_offset = 300
+
+    canvas_width = SCR_WIDTH - canvas_left_offset
+    canvas_height = SCR_HEIGHT - canvas_right_offset
+
+    canvas = pygame.Surface((canvas_width, canvas_height)).convert()
+    canvas.fill((255, 255, 255))
+    
+    rect_space = (canvas_width - rect_width * (BOARD_COLS + 1)) / BOARD_COLS
+
+    for x in range(BOARD_COLS + 1):
+        rectangle = pygame.Rect(x * (rect_space + rect_width), 0, rect_width, canvas_height)
+        pygame.draw.rect(canvas, (0, 0, 0), rectangle)
+    
+    rect_space = (canvas_height - rect_width * (BOARD_ROWS +1)) / BOARD_ROWS
+
+    for y in range(BOARD_ROWS + 1):
+        rectangle = pygame.Rect(0, y * (rect_space + rect_width), canvas_width, rect_width)
+        pygame.draw.rect(canvas, (0, 0, 0), rectangle)
+    
+    screen.blit(canvas , (canvas_left_offset / 2, canvas_right_offset / 2))
+    pygame.display.flip()
 
 def main():
+    DrawBoard(screen , None)
     ip = '127.0.0.1'
     port = 42069
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -58,13 +87,6 @@ def main():
             soc.close()
             break
         print(board)
-        
-
-
-
-
-
-
 
 if __name__ == '__main__':
     main()
--- Server.py
+++ Server.py
@@ -2,17 +2,19 @@ import socket
 
 class Board:
     def __init__(self):
-        self.board = [[0]*7 for x in range(6)]  
+        self.board = [[0]*7 for _ in range(6)]  
 
-    def getboarddata(self):
+    def GetBoardData(self):
         boardstring = ""
+
         for row in range(len(self.board)):
             for column in range(len(self.board[row])):
                 boardstring += str(self.board[row][column]) + ' '
             boardstring += '\n'
-        return  boardstring
+        
+        return boardstring
     
-    def addtoboard(self,player,column):
+    def AddToBoard(self,player,column):
         if 0 > column or column>=7:
             return False
         for row in range(len(self.board)-1, -1 , -1):
@@ -21,48 +23,46 @@ class Board:
                 return True 
         return False
 
-    def checkifwin(self):
-        winner = 0
+    def CheckIfWin(self):
         for row in self.board:
-            for i in range(len(row)-3):
+            for i in range(len(row) - 3):
                 if row[i] == row[i+1] == row[i+2] == row[i+3] != 0:
                     return row[i] 
-                    
-        for col in zip(*self.board):
-            for i in range(len(col)-3):
-                if col[i] == col[i+1] == col[i+2] == col[i+3] != 0:
-                    return col[i] 
 
-        for row in range(len(self.board)-3):
-            for col in range(row-3):
+        for col in zip(*self.board):
+            for i in range(len(col) - 3):
+                if col[i] == col[i+1] == col[i+2] == col[i+3] != 0:
+                    return col[i]
+
+        for row in range(len(self.board) - 3):
+            for col in range(row - 3):
                 if self.board[row][col] == self.board[row+1][col+1] == self.board[row+2][col+2] == self.board[row+3][col+3] != 0:
                     return self.board[row][col]
 
-        for row in range(len(self.board)-3, len(self.board)):
-            for col in range(row-3):
+        for row in range(len(self.board) - 3, len(self.board)):
+            for col in range(row - 3):
                 if self.board[row][col] == self.board[row-1][col+1] == self.board[row-2][col+2] == self.board[row-3][col+3] != 0:
                     return self.board[row][col]
         return 0
 
 class Player:
-    def __init__(self,addr, conn, ID):
+    def __init__(self, addr, conn, ID):
         self.addr = addr
         self.conn = conn
         self.ID = ID
 
 class Game:
-
     def __init__(self):
         self.player1 = None
         self.player2 = None
         self.turn = 1
         self.port = 42069
         self.ip = '127.0.0.1'
-        self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.board = Board()
+        self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.soc.bind((self.ip,self.port))
     
-    def StartGame(self):
+    def ConnectPlayers(self):
         self.soc.listen(2)
         conn1, addr1 = self.soc.accept()
         conn2, addr2 = self.soc.accept()
@@ -73,7 +73,7 @@ class Game:
         self.player1.conn.send(str(self.player1.ID).encode())
         self.player2.conn.send(str(self.player2.ID).encode())
 
-        self.GameLoop()
+        
 
     def GameLoop(self):
         while self.board.checkifwin() == 0 and '0' in self.board.getboarddata():
@@ -107,8 +107,8 @@ class Game:
 
 def main():
     game = Game()
-    game.StartGame()
-
+    game.ConnectPlayers()
+    game.GameLoop()
 
 if __name__ == '__main__':
     main()
